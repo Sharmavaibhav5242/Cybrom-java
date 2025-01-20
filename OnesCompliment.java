fc62cbeed4f8a1b0e921b6be3b27ef33391648f9
Sharmavaibhav5242 <vaibhavsharma5242@gmail.com>
2025-01-20 22:51:00 +0530

    password validation using simple ternary operator

--- OnesCompliment.java
+++ OnesCompliment.java
@@ -3,13 +3,24 @@ public class OnesCompliment
 {
 public static void main(String []args)
 {
+int password=5242;
+int Confirm;
+Scanner obj=new Scanner(System.in);
+System.out.println("Confirm Password");
+Confirm=obj.nextInt();
 
-int a=8;
-a=-a;
-System.out.println(a);
-int b=4;
-b=~b;
-System.out.println(b);
+String result;
+String result2;
+result=(password==Confirm)?"Open":"Wrong password";
+System.out.println(result);
+result2=(password!=Confirm)?"Enter correct password":"open";
+System.out.println(result2);
+int confirm;
+confirm=obj.nextInt();
+
+String finall;
+finall=(password==confirm)?"Open":"Wrong password";
+System.out.println(finall);
 
 }
 }
