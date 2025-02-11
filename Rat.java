ba41d10b9b587cc451c9521daa44bea20ca19d6c
Sharmavaibhav5242 <vaibhavsharma5242@gmail.com>
2025-02-11 23:13:53 +0530

    Diamond and number pattern

--- Rat.java
+++ Rat.java
@@ -1,52 +1,52 @@
 public class Rat {
     public static void main(String[] args) {
         for (int r = 1; r <= 5; ++r) {
-            for (int c = 1; c <= 5; ++c) {
-                if (c >= r) {
-                    System.out.print("*");
-                } else {
+            for (int c = 1; c <= 5; ++c) {      //*****
+                if (c >= r) {                   // ****
+                    System.out.print("*");      //  ***
+                } else {                         //  **
+                    System.out.print(" ");       //   *
+                }
+            }
+            System.out.println();
+        }
+        for (int r = 1; r <= 5; ++r) {         //*
+            for (int c = 1; c <= 5; ++c) {     //**
+                if (c <= r) {                  //***
+                    System.out.print("*");     //****
+                } else {                       //*****
                     System.out.print(" ");
                 }
             }
             System.out.println();
         }
         for (int r = 1; r <= 5; ++r) {
-            for (int c = 1; c <= 5; ++c) {
-                if (c <= r) {
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
-                }
+            for (int c = 1; c <= 5; ++c) {    //******
+                if (c <= 6 - r) {             //*****
+                    System.out.print("*");    //****
+                } else {                      //***
+                    System.out.print(" ");    //**
+                }                             //*
             }
             System.out.println();
         }
         for (int r = 1; r <= 5; ++r) {
-            for (int c = 1; c <= 5; ++c) {
-                if (c <= 6 - r) {
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
-                }
-            }
-            System.out.println();
-        }
-        for (int r = 1; r <= 5; ++r) {
-            for (int c = 1; c <= 5; ++c) {
-                if (c >= 6 - r) {
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
+            for (int c = 1; c <= 5; ++c) {         //*
+                if (c >= 6 - r) {                 //**
+                    System.out.print("*");       //***
+                } else {                        //****
+                    System.out.print(" ");     //*****
                 }
             }
             System.out.println();
         }
         for(int r=1;r<=5;r++)
      {
-         for(int c=1;c<=9;c++)
-         {
-             if(c>=6-r &&  c<=4+r)
-             {
-                 System.out.print("*");
+         for(int c=1;c<=9;c++)              //*
+         {                                 //***
+             if(c>=6-r &&  c<=4+r)        //*****
+             {                           //*******
+                 System.out.print("*"); //*********
 
              }
              else
@@ -56,5 +56,52 @@ public class Rat {
          }
          System.out.println();
      }
+        int a=0;
+        for (int r=1;r<10;++r)
+        {
+            int i=(r<=5)?a++:a--;
+            for (int c=1;c<10;++c)
+            {
+               if(c>=6-a && c<=4+a)
+               {
+                   System.out.print("*");
+               }
+               else
+               {
+                   System.out.print(" ");
+               }
+            }
+            System.out.println();
+        }
+        for (int r=1;r<=5;++r)
+        {
+            for (int c=1;c<=5;++c)
+            {
+                if (c<=r)
+                {
+                    System.out.print(r);
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        for (int r=1;r<=5;++r)
+        {
+            for (int c=1;c<=5;++c)
+            {
+                if (c<=r)
+                {
+                    System.out.print(c);
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
