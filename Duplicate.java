b33ff670a78e78079930429c50f2efae8b49c963
Sharmavaibhav5242 <vaibhavsharma5242@gmail.com>
2025-02-18 22:59:20 +0530

    Unique value

--- Duplicate.java
+++ Duplicate.java
@@ -3,10 +3,46 @@ public class Duplicate {
     public static void main(String[] args) {
 
                 Scanner obj=new Scanner (System.in);
-                int arr[]=new int[]{1,2,1,2,3,3,111,2};
+//                int arr[]=new int[]{1,2,1,2,3,3,111,2};
+//
+//                for(int i=0; i<arr.length; ++i){
+//                    int count=1;
+//                    boolean dup=false;
+//
+//
+//                    for(int j=i+1; j<arr.length; j++)
+//                    {
+//
+//
+//                        if(arr[i]==arr[j]){
+//                            arr[j]=-1;
+//                            dup=true;
+//                            count++;
+//                        }
+//
+//                    }
+//
+//                    if(dup && arr[i]!=-1 ){
+//                        System.out.println(count);
+//                    }
+//                    else if(arr[i]!=-1){
+//                        System.out.println(count);
+//
+//
+//
+//                    }
+//                }
 
-                for(int i=0; i<arr.length; ++i){
-                    int count=1;
+                int arr[]=new int[]{1,2,1,2,3,3,111,2};
+                int min=0;
+
+                for(int i=0; i<arr.length-1; ++i){
+
+                    if(arr[i]<min){
+                        min=arr[i];
+                    }
+                }
+                for(int i=0; i<arr.length-1; ++i){
                     boolean dup=false;
 
 
@@ -15,25 +51,16 @@ public class Duplicate {
 
 
                         if(arr[i]==arr[j]){
-                            arr[j]=-1;
-                            dup=true;
-                            count++;
-                        }
+                            arr[j]=min-1;
+                            dup=true;        }
 
                     }
 
-                    if(dup && arr[i]!=-1 ){
-                        System.out.println(count);
-                    }
-                    else if(arr[i]!=-1){
-                        System.out.println(count);
+                    if(!dup && arr[i]!=min-1 ){
+                        System.out.println(arr[i]);
+                    }}
 
-
-
-                    }
-                }
-
-
+            }
         }
-    }
+
 
