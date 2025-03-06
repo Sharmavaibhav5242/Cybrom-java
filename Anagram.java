14b5b429d037aba17c49be6ae74515175a8e578d
Sharmavaibhav5242 <vaibhavsharma5242@gmail.com>
2025-03-06 19:03:32 +0530

    subset of string

--- Anagram.java
+++ Anagram.java
@@ -1,10 +1,41 @@
 import java.util.Scanner;
 public class Anagram {
-    public static void main(String[] args) {
-        Scanner obj=new Scanner(System.in);
-        String str="heart";
-        String str1="earth";
-        int[] count=new int[256];
+    static boolean sub(String str,String str1)
+    {
+        char ch=0;
+        char ch1=0;
+        int num=0;
+        for (int i=0;i<str.length();++i)
+        {
+            ch=str.charAt(i);
+            for (int j=0;j<str1.length();++j)
+            {
+                ch1=str1.charAt(j);
+                if (ch==ch1){
+                    num++;
+                }
+            }
+        }
 
+        if (num!=str1.length())
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    public static void main(String[] args) {
+        String str="ABCDE";
+        String str1="ABC";
+        if (sub(str,str1))
+        {
+            System.out.println("Yes");
+        }
+        else
+        {
+            System.out.println("No");
+        }
     }
 }
