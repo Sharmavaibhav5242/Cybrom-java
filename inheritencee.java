7a21a8b84c000a39c653dd464cdd531d87117fec
Sharmavaibhav5242 <vaibhavsharma5242@gmail.com>
2025-03-31 18:44:50 +0530

    Single inheritence

--- inheritencee.java
+++ inheritencee.java
@@ -1,4 +1,13 @@
-public class inheritencee {
+class two
+{
+    void show()
+    {
+        System.out.println("Hello");
+    }
+}
+public class inheritencee extends two {
     public static void main(String[] args) {
+        inheritencee obj=new inheritencee();
+        obj.show();
     }
 }
